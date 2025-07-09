53f1849511151d3f5a66d728b1cf53a29465b6b6
KhurramAdeel <146566170+KhurramAdeel@users.noreply.github.com>
2025-07-09 22:54:00 +0500

    - added controller and it's test class

--- demo/src/main/java/com/learn/githubactions/springboot/demo/controller/HelloWorldController.java
+++ demo/src/main/java/com/learn/githubactions/springboot/demo/controller/HelloWorldController.java
@@ -1,2 +1,14 @@
-package com.learn.githubactions.springboot.demo.controller;public class HelloWorldController {
+package com.learn.githubactions.springboot.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping( value= "/hello")
+    public String helloWorld(){
+        return "hello world";
+    }
+
 }
--- demo/src/test/java/com/learn/githubactions/springboot/demo/controller/HelloWorldControllerTest.java
+++ demo/src/test/java/com/learn/githubactions/springboot/demo/controller/HelloWorldControllerTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.learn.githubactions.springboot.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
 class HelloWorldControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHelloWorldEndpoint() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello world"));
+    }
+
+
 }
